7a9e0444c8121345bf00975605b60757acd29848
Liviu <liviu2002liviu@gmai.com>
2021-11-24 12:48:06 +0200

    BG is working, finnally

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+
+const imageBackground =require("./Images/backgound_blue.jpg");
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <ImageBackground source={imageBackground} style={styles.image}>
+        <View style={styles.container}>
+            <Text>Testing background</Text>
+            <StatusBar style="auto" />
+        </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+    image:{
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
+    },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
