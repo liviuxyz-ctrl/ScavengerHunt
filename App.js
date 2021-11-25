52b8e1f0733115ee2879ce8fa7b4a6fd380553b2
Liviu <liviu2002liviu@gmai.com>
2021-11-25 22:11:50 +0200

    Still no background

--- App.js
+++ App.js
@@ -3,10 +3,8 @@ import React from 'react';
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import { ImageBackground, StyleSheet, Text, View } from 'react-native';
-import {Color} from './constants/Colors';
-import {imageBackground} from './constants/FilePaths';
-import {CustomButton} from "./components/CustomButton";
 import {HomeScreen} from "./screens/HomeScreen";
+import {imageBackground} from "./constants/FilePaths";
 
 
 const Stack = createNativeStackNavigator();
@@ -14,37 +12,42 @@ const Stack = createNativeStackNavigator();
 const StackNavigator = () => {
 
     return <Stack.Navigator>
-        <Stack.Screen name={'Home'} component={HomeScreen}/>
-    </Stack.Navigator>
+                    <Stack.Screen name={'Home'} component={HomeScreen}/>
+        </Stack.Navigator>
 }
 
 export default function App() {
-  return (
-      <ImageBackground source={imageBackground} style={styles.image}>
-          <NavigationContainer style={styles.container}>
-                    <StatusBar hidden/>
-                    <StackNavigator/>
-          </NavigationContainer>
+  return ( <ImageBackground source={imageBackground} style={styles.image}>
+      <View style={styles.container}>
+              <NavigationContainer>
+                  <StatusBar hidden/>
+                  <StackNavigator style={styles.container}  />
+              </NavigationContainer>
+      </View>
       </ImageBackground>
+
   );
 }
 
 const styles = StyleSheet.create({
     image:{
-        flex: 1,
         resizeMode: 'cover',
+        alignItems: 'center',
         justifyContent: 'center',
+        position: 'relative'
     },
   container: {
     alignItems: 'center',
     justifyContent: 'center',
+      // position: 'absolute',
+      elevation: 10,
   },
     titleText :{
         color : 'white',
         fontSize: 25,
         paddingBottom: 100,
+    },
 
-    }
 
 });
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Button, View} from "react-native";
+import {View} from "react-native";
 import {CustomButton} from "../components/CustomButton";
 
+
 export const HomeScreen= ({navigation}) =>{
-    return <View style={{flex:1}}>
-        <CustomButton unfilled title={'Scan QR code'} onPress={() => navigation.navigate('QRScannerScreen')}/>
-        <CustomButton unfilled title={'Sponsors'} onPress={() => navigation.navigate('SponsorsScreen')}/>
-        </View>
+    return <View>
+                    <CustomButton unfilled title={'Scan QR code'} onPress={() => navigation.navigate('QRScannerScreen')}/>
+                    <CustomButton unfilled title={'Sponsors'} onPress={() => navigation.navigate('SponsorsScreen')}/>
+            </View>
 }
